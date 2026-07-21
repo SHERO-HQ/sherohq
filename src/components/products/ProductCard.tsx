dd0cc0700f5b57f9ba5a38646d05302a76ca1632
realib1 <ibeevpersie@gmail.com>
2026-07-21 11:22:59 -0700

    Updated font size of badges

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -144,17 +144,17 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, onQuickView }) => {
         {/* Badges (Bottom Left) */}
         <div className="absolute bottom-3 left-3 z-20 flex flex-wrap gap-1">
           {product.badge && (
-            <span className="px-2.5 py-1 rounded text-[11px] font-semibold uppercase tracking-tight bg-brand-secondary-600 text-white">
+            <span className="px-2.5 py-1 rounded text-[9px] font-semibold uppercase tracking-tight bg-brand-secondary-600 text-white">
               {product.badge}
             </span>
           )}
           {discount > 0 && (
-            <span className="px-2.5 py-1 rounded text-[11px] font-semibold uppercase tracking-tight bg-red-600 text-white">
+            <span className="px-2.5 py-1 rounded text-[9px] font-semibold uppercase tracking-tight bg-red-600 text-white">
               -{discount}%
             </span>
           )}
           {!product.inStock && (
-            <span className="px-2.5 py-1 rounded text-[11px] font-semibold uppercase tracking-tight bg-slate-900/80 text-white ">
+            <span className="px-2.5 py-1 rounded text-[9px] font-semibold uppercase tracking-tight bg-slate-900/80 text-white ">
               Sold Out
             </span>
           )}
@@ -198,12 +198,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, onQuickView }) => {
                 {formatCurrency(product.originalPrice)}
               </span>
             )}
-            <div className="flex items-center justify-between gap-4">
+            <div className="flex flex-col-reverse justify-between gap-4">
               <span className="text-lg sm:text-xl font-semibold dark:text-white text-slate-900 leading-none">
                 {formatCurrency(product.price)}
               </span>
               {!product.inStock && (
-                <span className="shrink-0 text-[10px] font-bold uppercase text-red-500 bg-red-500/10 px-2 py-1 rounded border border-red-500/20">
+                <span className="shrink-0 text-[8px] font-bold text-red-500 bg-red-500/10 px-2 py-0.5 rounded border border-red-500/20 w-fit">
                   Out of Stock
                 </span>
               )}
